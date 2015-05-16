86e01c8bd5706b6d051f826ba49c64fdfd3d0fbd
kunjk23 <kunjk23@users.noreply.github.com>
2015-05-16 21:03:34 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,44 @@
+package com.fl.kunj.fibclock;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        DrawView drawView = new DrawView(this);
+        drawView.setBackgroundColor(Color.BLACK);
+        setContentView(drawView);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
